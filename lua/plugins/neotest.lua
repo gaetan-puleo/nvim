83bc4100d9cb0d80c6f6f29ea809cd5feccf020b
Gaetan Puleo <pro@gaetanpuleo.com>
2023-10-02 23:19:04 +0200

    feat(neotest): add new keymap

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -15,3 +15,4 @@ local map = vim.api.nvim_set_keymap
 
 vim.keymap.set('n', '<leader>tf', "<Cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", {desc = '[T]est [F]ile'})
 vim.keymap.set('n', '<leader>tf', "<Cmd>lua require('neotest').run.run())<CR>", {desc = '[T]est [N]earest'})
+vim.keymap.set('n', '<leader>ts', "<Cmd>Neotest summary <CR>", {desc = '[T]oggle [S]ummary'})
