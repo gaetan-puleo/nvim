9c2df2de7b917d0d5138cf5dee7d6b1985675407
Gaetan Puleo <pro@gaetanpuleo.com>
2023-10-04 00:51:37 +0200

    fix: use mistral

--- lua/plugins/gen-nvim.lua
+++ lua/plugins/gen-nvim.lua
@@ -1,2 +1,4 @@
-vim.keymap.set('v', '<leader>v', ':Gen<CR>')
-vim.keymap.set('n', '<leader>v', ':Gen<CR>')
+vim.keymap.set('v', '<leader>ai', ':Gen<CR>')
+vim.keymap.set('n', '<leader>ai', ':Gen<CR>')
+
+-- require('gen').model = 'codellama:34b' -- default 'mistal:instruct'
